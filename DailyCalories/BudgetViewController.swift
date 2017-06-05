b2ca6bd068b995a8b0f03206d513d47d3ceaae01
Chris Winstanley <chris.winstanley@bbc.co.uk>
2017-06-05 09:45:10 +0100

    Initial commit

---
+++ DailyCalories/BudgetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BudgetViewController.swift
+//  DailyCalories
+//
+//  Created by Chris Winstanley on 05/06/2017.
+//  Copyright © 2017 Chris Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
